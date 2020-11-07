ee356e8789427afe6dd94178cd33e1ec773e669c
Beth Marshall <beth.marshall@askherconsulting.co.uk>
2020-11-07 22:14:35 +0000

    initial commit with single working test

---
+++ Royale/Pages/PageBase.cs
@@ -0,0 +1,14 @@
+﻿using OpenQA.Selenium;
+
+namespace Royale.Pages
+{
+    public abstract class PageBase
+    {
+        public readonly HeaderNav HeaderNav;
+
+        public PageBase(IWebDriver driver)
+        {
+            HeaderNav = new HeaderNav(driver);
+        }
+    }
+}
